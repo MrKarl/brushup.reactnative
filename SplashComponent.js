262270ae09d882af7a49ae546ab412d1039733c1
KarlPark <karl.panki.park@gmail.com>
2018-10-18 17:10:28 +0900

    Add SplashComponent
    - 스플래시 화면 으로 intro 대체
    - TODO : setInterval 등을 통해서 다음 컴포넌트로 가도록 수정해야함

---
+++ SplashComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class SplashComponent extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>Brushup</Text>
+        <Text style={styles.subText}>영어 고수가 되는 방법</Text>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  logo: {
+    fontSize: 40,
+    textAlign: 'center',
+    margin: 20,
+  },
+  subText: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
